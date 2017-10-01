dd7d58b5b96b0d23f9c8171c0d3005e9e48a6a13
yarshad <yarshad22@gmail.com>
2017-09-30 22:58:06 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { AgGridModule }  from "ag-grid-angular";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    AgGridModule.withComponents([ ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
